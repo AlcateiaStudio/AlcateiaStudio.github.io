ccf0b5c03e0dc60310333a99ff9b2ce4272e0c2c
Andress <andressmartin.aka@gmail.com>
2023-12-13 17:06:51 -0300

    Updated Contact form

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
+require('dotenv').config();
 
 // server used to send send emails
 const app = express();
@@ -15,8 +16,8 @@ console.log(process.env.EMAIL_PASS);
 const contactEmail = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: "********@gmail.com",
-    pass: ""
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
   },
 });
 
@@ -32,14 +33,14 @@ router.post("/contact", (req, res) => {
   const name = req.body.firstName + req.body.lastName;
   const email = req.body.email;
   const message = req.body.message;
-  const phone = req.body.phone;
+  const subject = req.body.subject;
   const mail = {
     from: name,
-    to: "********@gmail.com",
-    subject: "Contact Form Submission - Portfolio",
+    to: process.env.EMAIL_USER,
+    subject: subject,
     html: `<p>Name: ${name}</p>
            <p>Email: ${email}</p>
-           <p>Phone: ${phone}</p>
+           <p>Subject: ${subject}</p>
            <p>Message: ${message}</p>`,
   };
   contactEmail.sendMail(mail, (error) => {
